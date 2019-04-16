52dd8b5f331104d3787e2fc465c8c170a7419874
Tiago Rocha <tiago.aziz@gmail.com>
2019-04-16 20:53:06 +0100

    Initial Commit

---
+++ SpeechToText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToText
+//
+//  Created by Tiago Rocha on 16/04/2019.
+//  Copyright © 2019 Tiago Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
